79b9d8831900a0458181f0e5781bfdee6df5de2d
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-04-05 18:54:31 +0000

    add  mode payment

--- panier/payment.php
+++ panier/payment.php
@@ -1,39 +1,46 @@
-<?php
-$paypal_url='https://www.sandbox.paypal.com/cgi-bin/webscr'; // URL de l'API Paypal de teste
-$paypal_id='EQ7UGS7DEHSFL'; // Business email ID
-$client="client";
-$prix=90;
-?>
-<h4>Bien venu, <?php echo $client; ?></h4>
+<div id="smart-button-container">
+      <div style="text-align: center;">
+        <div id="paypal-button-container"></div>
+      </div>
+    </div>
+  <script src="https://www.paypal.com/sdk/js?client-id=sb&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
+  <script>
+    function initPayPalButton() {
+      paypal.Buttons({
+        style: {
+          shape: 'rect',
+          color: 'gold',
+          layout: 'vertical',
+          label: 'paypal',
+          
+        },
 
-<div class="product">            
-	<div class="image">
-		<a href="http://www.oujood.com/" title="Cours et tutoriels  en ligne pour apprendre le développement Web" style="text-decoration: none;" rel="dofollow"><img src="http://www.oujood.com/images/logo.png" /></a>
-	</div>
-	<div class="name">
-		PHP_OUJOOD Payement
-	</div>
-	<div class="price">
-		Prix:<?php echo $prix; ?>
-	</div>
-	<div class="btn">
-	<form action="<?php echo $paypal_url; ?>" method="post" name="frmPayPal1">
-	<input type="hidden" name="business" value="<?php echo $paypal_id; ?>">
-	<input type="hidden" name="cmd" value="_xclick">
-	<input type="hidden" name="item_name" value="OUJOOD Payment">
-	<input type="hidden" name="item_number" value="1">
-	<input type="hidden" name="credits" value="510">
-	<input type="hidden" name="userid" value="1">
-	<input type="hidden" name="amount" value="<?php echo $prix; ?>">
-	<input type="hidden" name="cpp_header_image" value="http://www.oujood.com/images/logo.png">
-	<input type="hidden" name="no_shipping" value="1">
-	<input type="hidden" name="currency_code" value="USD">
-	<input type="hidden" name="handling" value="0">
-	<input type="hidden" name="cancel_return" value="http://localhost/php-paypal/cancel.php">
-	<input type="hidden" name="return" value="http://localhost/php-paypal/success.php?tx=83437E384950D&st=Completed&amt=90.00&cc=USD&cm=&item_number=1">
-	<input type="image" src="https://www.sandbox.paypal.com/fr_FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - Le moyen le plus sûr et le plus simple de payer en ligne !">
-	<img alt=" PayPal - The safer, easier way to pay online!" border="0" src="https://www.sandbox.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-	</form> 
-	</div>
-</div>
- 
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
+          });
+        },
+
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
+            
+            // Full available details
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+
+            // Show a success message within this page, e.g.
+            const element = document.getElementById('paypal-button-container');
+            element.innerHTML = '';
+            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+
+            // Or go to another URL:  actions.redirect('thank_you.html');
+            
+          });
+        },
+
+        onError: function(err) {
+          console.log(err);
+        }
+      }).render('#paypal-button-container');
+    }
+    initPayPalButton();
+  </script>
